f1ac6a1a80af6b1a2741e4f158a29ea891938227
rurigrass <ruripg@gmail.com>
2020-11-08 23:03:50 +0100

    first commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react"
+import ReactDOM from "react-dom"
+import { ThemeProvider } from "styled-components"
+
+import { Card, Content, Grid, Title } from "./components"
+import reportWebVitals from "./core/report-web-vitals"
+import { GlobalStyles, theme } from "./styles"
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <ThemeProvider theme={theme}>
+    <GlobalStyles />
+    <Content>
+      <Title>Sudoku</Title>
+      <Card>
+        <Grid></Grid>
+      </Card>
+    </Content>
+  </ThemeProvider>,
+  document.getElementById("root")
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
